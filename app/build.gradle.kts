5fc10039db2c7d5c4562117322a8864b4ffb60b2
mti-prog <personalofmti@gmail.com>
2025-10-22 22:18:11 +0600

    hw6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
 dependencies {
 
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:3.0.0")
-    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.8.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.8.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     //Material Design
